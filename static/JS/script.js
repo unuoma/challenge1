87e65e81a7b73b53b7a93e83d78df3f59731dd0c
Unuoma <obanyamaria@gmail.com>
2020-08-11 01:01:59 -0400

    challenge 1

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,2 +1,16 @@
-//alert('hello');
-console.log('hello');
+//challenge 1: Age in days
+function ageInDays(){
+    var birthYear = prompt('what year were you born?');
+    var ageIDayz = (2020 - birthYear) * 365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + ageIDayz + ' days old.');
+    h1.setAttribute('id', 'ageInDayz');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset() {
+    document.getElementById('ageInDayz').remove();
+}
+
+
