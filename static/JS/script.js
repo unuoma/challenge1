f274658b24a2cd02d520fa1d25311ab4010eda44
Unuoma <obanyamaria@gmail.com>
2020-08-18 22:31:09 -0400

    challenge 2

--- static/JS/script.js
+++ static/JS/script.js
@@ -13,4 +13,11 @@ function reset() {
     document.getElementById('ageInDayz').remove();
 }
 
+function generateCat () {
+    let image = document.createElement('img');
+    let div = document.getElementById('flex-cat-gen');
+    image.src = "https://thecatapi.com/api/images/get?format=src&type=gif&size=small"
+    div.appendChild(image);
+}
+
 
