df1e731111a0a97d45bc1d9bb3316af6b5b90809
Unuoma <obanyamaria@gmail.com>
2020-08-19 21:10:35 -0400

    challenge 3

--- static/JS/script.js
+++ static/JS/script.js
@@ -12,7 +12,7 @@ function ageInDays(){
 function reset() {
     document.getElementById('ageInDayz').remove();
 }
-
+//Cat generator
 function generateCat () {
     let image = document.createElement('img');
     let div = document.getElementById('flex-cat-gen');
@@ -20,4 +20,72 @@ function generateCat () {
     div.appendChild(image);
 }
 
+//RPS challenge 3
+function rpsGame(yourChoice) {
+    console.log(yourChoice);
 
+    let humanChoice, botChoice;
+    humanChoice = yourChoice.id;
+
+    botChoice = numberToChoice(randomRPSInt());
+    console.log('Computer choice:', botChoice);
+
+    results = decideWinner(humanChoice, botChoice); //[o, 1] human lost | bot won
+    console.log(results);
+
+    message = finalMessage(results); // {'message': 'you won!', 'color': 'green'}
+    console.log(message)
+    rpsFrontEnd(yourChoice.id, botChoice, message);
+}
+
+function randomRPSInt() {
+    return Math.floor(Math.random() * 3);
+}
+function numberToChoice(number) {
+    return ['rock', 'paper', 'scissors'][number];
+}
+function decideWinner(yourChoice, botChoice) {
+    let rpsDatabase = {
+        'rock': {'scissors': 1, 'rock': 0.5, 'paper': 0},
+        'paper': {'scissors': 0, 'rock': 1, 'paper': 0.5},
+        'scissors': {'scissors': 0.5, 'rock': 0, 'paper': 1},
+    }
+    let yourScore = rpsDatabase[yourChoice][botChoice];
+    let computerScore = rpsDatabase[botChoice][yourChoice];
+
+    return [yourScore, computerScore];
+}
+
+function finalMessage([yourScore, computerScore]) {
+    if(yourScore === 0) {
+        return {'message': 'you lost!', 'color': 'red'};
+    }  else if (yourScore === 0.5) {
+        return {'message': 'you tied!', 'color': 'yellow'};
+    } else {
+        return {'message': 'you Won!', 'color': 'green'};
+    }
+}
+
+function rpsFrontEnd(humanImage, botImage, finalMessage) {
+    let imagesDatabase = {
+        'rock': document.getElementById('rock').src,
+        'paper': document.getElementById('paper').src,
+        'scissors': document.getElementById('scissors').src,
+    }
+    document.getElementById('rock').remove();
+    document.getElementById('paper').remove();
+    document.getElementById('scissors').remove();
+
+    let humanDiv = document.createElement('div');
+    let botDiv = document.createElement('div');
+    let messageDiv = document.createElement('div');
+
+    humanDiv.innerHTML = "<img src='" + imagesDatabase[humanImage] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(37, 50, 233,1);'>"
+    messageDiv.innerHTML = "<h1 style='color: " + finalMessage['color'] + "; font-size: 60px; padding: 30px; '>" + finalMessage['message'] + "</h1>"
+    botDiv.innerHTML = "<img src='" + imagesDatabase[botImage] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(243, 38, 24,1);'>"
+
+    document.getElementById('flex-box-rps-div').appendChild(humanDiv);
+    document.getElementById('flex-box-rps-div').appendChild(messageDiv);
+    document.getElementById('flex-box-rps-div').appendChild(botDiv);
+
+}
