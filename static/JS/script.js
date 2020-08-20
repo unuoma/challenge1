7a51ee6b96cdfb1ec60bfc04f2ebc36d8ee14bc7
Unuoma <obanyamaria@gmail.com>
2020-08-19 23:08:32 -0400

    challenge 4 - color

--- static/JS/script.js
+++ static/JS/script.js
@@ -89,3 +89,56 @@ function rpsFrontEnd(humanImage, botImage, finalMessage) {
     document.getElementById('flex-box-rps-div').appendChild(botDiv);
 
 }
+
+//challenge 4 - color buttons
+let all_buttons = document.getElementsByTagName('button');
+
+let copyAllButtons = [];
+for (let i=0; i < all_buttons.length; i++) {
+    copyAllButtons.push(all_buttons[i].classList[1]);
+    console.log(copyAllButtons);
+}
+
+
+function buttonColorChange (buttonThing) {
+    if (buttonThing.value === 'Red') {
+        buttonsRed();
+    } else if (buttonThing.value === 'Green') {
+        buttonsGreen();
+    } else if (buttonThing.value === 'Reset') {
+        buttonColorReset();
+    } else if (buttonThing.value === 'Random') {
+        randomColors();
+    }
+}
+
+function buttonsRed() {
+    for (let i=0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-danger');
+    }
+}
+
+function buttonsGreen() {
+    for (let i=0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-success');
+    }
+}
+
+function buttonColorReset() {
+    for (let i=0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButtons[i]);
+    }
+}
+
+function randomColors() {
+    let choices = ["btn-primary", "btn-danger", "btn-success", "btn-warning"]
+    for (let i=0; i < all_buttons.length; i++) {
+        let randomNumber = Math.floor(Math.random() * 4);
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(choices[randomNumber])
+    }
+}
+
